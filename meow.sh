1c793e536f4e22a6c005d519c845f9935ee9b760
Fiona Klute <fiona.klute@gmx.de>
2020-09-19 13:39:26 +0200

    Meow multiple times if provided with a number

--- meow.sh
+++ meow.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-echo "Meow! =^.^="
+if echo "${1}" | egrep -q '^[0-9]+$'; then
+    m="${1}"
+else
+    m=1
+fi
+
+i=0
+while [ $i -lt $m ]; do
+    echo -n "Meow! "
+    i=$((i + 1))
+done
+echo "=^.^="
